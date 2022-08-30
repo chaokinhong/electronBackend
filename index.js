4edaba2fb4623f581ccf499992ac4069f4c40d8f
chaokinhong <3484715033@qq.com>
2022-08-30 15:48:18 +0100

    'new'

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen( 5000 ,() => console.log(`Server running on port: ${PORT}`))
+    app.listen( PORT ,() => console.log(`Server running on port: ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
